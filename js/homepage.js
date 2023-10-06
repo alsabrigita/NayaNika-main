bb312d862b11e4465fc4d3ad5fea3b1bdacd08bd
sdnafisah <sd.nafisahh@gmail.com>
2023-10-06 16:50:16 +0700

    update

--- js/homepage.js
+++ js/homepage.js
@@ -1,25 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Function to toggle active class on navigation links
-    const navLinks = document.querySelectorAll("nav ul li a");
-    navLinks.forEach(link => {
-      link.addEventListener("click", function() {
-        navLinks.forEach(link => link.classList.remove("active"));
-        this.classList.add("active");
-      });
-    });
-  
-    // Function to handle click event on "Shop Now" button
-    const shopNowButton = document.querySelector(".herobutton button");
-    shopNowButton.addEventListener("click", function() {
-      window.location.href = "shop.html";
-    });
-  
-    // Function to handle click event on "View All" buttons
-    const viewAllButtons = document.querySelectorAll(".viewallbutton button");
-    viewAllButtons.forEach(button => {
-      button.addEventListener("click", function() {
-        window.location.href = "shop.html";
-      });
-    });
-  });
-  
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu");
+
+menuBar.addEventListener("click", () => {
+    menuNav.classList.toggle("menu-active");
+})
+
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    const windowPosition = window.scrollY > 0;
+    navBar.classList.toggle("scrolling-active", windowPosition)
+
+    menuNav.classList.remove("menu-active");
+})
